31305885d779daeb1d8271fefae1bb3e235722ee
Zaheer Ismail Shaikh <105771794+Xaheersays@users.noreply.github.com>
2022-07-26 09:36:26 +0530

    Update binaryTree.py

--- binaryTree.py
+++ binaryTree.py
@@ -17,15 +17,7 @@ print(a.left.data)
 print(a.right.data)
 
 
-d=Node("d")
-e=Node("e")
-f=Node("f")
-g=Node("g")
-c=Node("c",f,g)
-b=Node("b",d,e)
-a=Node("a",b,c)
-
-
+a=Node("a",Node("b",Node("d"),Node("e")),Node("c",Node("f"),Node("g")))
 
 
 a.data
@@ -49,4 +41,61 @@ a.right.right.data
 
 
 
+def inorder(root):
+    if root :
+        inorder(root.left)
+        print(root.data)
+        inorder(root.right)
+inorder(a)        
+
+# ans<<<    
+def inorder(root):
+    if root :
+        inorder(root.left)
+        print(root.data)
+        inorder(root.right)
+inorder(a)        
+d
+b
+e
+a
+f
+c
+g
+
+
+inorder(a)        
+def inorder(root):
+    if root :
+        inorder(root.left)
+        print(root.data)
+        inorder(root.right)
+inorder(a)   
+#ans<<<
+
+d
+b
+e
+a
+f
+c
+g
+
+
+def postorder(root):
+    if root :
+        preorder(root.left)
+        preorder(root.right)
+        print(root.data)
+        
+preorder(a)
+#ans<<<
+
+a
+b
+d
+e
+c
+f
+g
 
